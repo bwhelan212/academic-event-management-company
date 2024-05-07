891bd4b3375c649759f6f3704dbc8361adc48f7e
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-05-07 12:02:58 -0700

    connected login to db, user can log in w/email + pass

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     //data from form post
     $email= $_POST['email'] ?? '';
@@ -19,12 +21,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     //add password confirmation if we want
 
-    //if valid insert the email and password into db
+   // retrieve query from db for login and if in db go to event-page
     if($isvalid) {
         define("DB_SERVER", "localhost");
-        define("DB_USERNAME","root");
+        define("DB_USERNAME", "root");
         define("DB_PASSWORD", "");
-        define("DB_NAME", "company");
+        define("DB_NAME", "AEM2");
 
         $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
@@ -34,6 +36,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             $error_msg .= " (" . mysqli_connect_errno() . ")";
             exit($error_msg);
         }
+
+        $query = "SELECT Email, User_password FROM USER_DETAILS WHERE Email=? and User_password=?";
+        $stmt = mysqli_prepare($db, $query);
+        mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $row = mysqli_num_rows($result);
+        $count = mysqli_num_rows($result);
+
+        if($count == 1) {
+            $_SESSION['email_user'] = $email;
+            header('location: event-page.php');
+        } else {
+            $error = "Your Email or Password is invalid";
+            echo "invalid email or pass";
+        }
+
     }
 }
 ?>
